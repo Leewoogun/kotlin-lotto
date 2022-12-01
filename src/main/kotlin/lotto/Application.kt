eab6ec5e8184bb2d727f225413b610f3ebe8d201
woogun <bome519@naver.com>
2022-12-01 16:48:33 +0900

    Feat: 로또 구입 금액 기능, 로또 금액 예외처리 기능, 로또 갯수 기능, 로또 갯수 만큼 번호 받기 기능 추가
    1. 로또 구입 금액 기능 : readLine 함수를 통해 String? 형태로 입력을 받아옴
    2. 로또 금액 예외처리 : 사용자의 입력이 빈칸이거나 1000원으로 나누어떨어지지 않을 경우 throw를 통해 IllegalArgumentException 예외를 던짐
    3. 로또 갯수 기능 : 입력에서 1000으로 나누었을 때 몫을 return 받음
    4. 로또 갯수 만큼 번호 받기 기능 : 프로젝트에서 제시한 Randoms API를 import 하여 몫의 수만큼 반복문을 통해 2차원 리스트로 만듬

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,46 @@
 package lotto
 
+import java.lang.IllegalArgumentException
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+
 fun main() {
-    TODO("프로그램 구현")
+    inputLottoNum()
 }
+
+fun inputLottoNum(){
+    println("구입금액을 입력해 주세요.")
+    val inputNum = readLine()
+    inputException(inputNum)
+    val userLottoCount = lottoCount(inputNum)
+    println("${userLottoCount}개를 구매했습니다.")
+    val userLottoNum = lottoNumber((userLottoCount))
+}
+
+fun inputException(input : String?){
+    if (input?.length == 0){
+        throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
+        return
+    }
+
+    if (input!!.toInt() % 1000 != 0 ){
+        throw IllegalArgumentException("[ERROR] 잘못된 입력입니다.")
+        return
+    }
+}
+
+fun lottoCount(input : String?) : Int{
+    return input!!.toInt() / 1000
+}
+
+fun lottoNumber(count : Int) : MutableList<MutableList<Int>>{
+    val numberList = mutableListOf<MutableList<Int>>()
+    val numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6)
+
+    for (i in 1..count){
+        numberList.add(numbers)
+    }
+    return numberList
+}
+
